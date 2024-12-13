b31f60fbb3022fa51b5f35fae8af06b536f55d52
Tesshu Flower <tflower@redhat.com>
2024-12-13 17:35:39 -0500

    WIP - tests, helmupdates
    Signed-off-by: Tesshu Flower <tflower@redhat.com>

--- controllers/helmutils/helmutils_test.go
+++ controllers/helmutils/helmutils_test.go
@@ -1,41 +1,35 @@
 package helmutils_test
 
 import (
+	"strings"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/klog/v2"
 	clusterv1 "open-cluster-management.io/api/cluster/v1"
 
+	"github.com/stolostron/volsync-addon-controller/controllers"
 	"github.com/stolostron/volsync-addon-controller/controllers/helmutils"
+	"github.com/stolostron/volsync-addon-controller/controllers/helmutils/helmutilstest"
 )
 
 var _ = Describe("Helmutils", func() {
+	logger := zap.New(zap.UseDevMode(true), zap.WriteTo(GinkgoWriter))
+
 	Context("Load embedded helm charts", func() {
 		// see the test suite BeforeSuite() for initing the charts with our test charts
-		It("Should have stable chart loaded", func() {
+		It("Should have stable-X.Y chart loaded", func() {
 			// Stable chart should always exist
-			chart, err := helmutils.GetEmbeddedChart("stable")
+			chart, err := helmutils.GetEmbeddedChart(controllers.DefaultHelmChartKey)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(chart).NotTo(BeNil())
 
-			// our test charts stable is v0.10.0
-			Expect(chart.AppVersion()).To(Equal("0.10.0"))
-		})
-
-		It("Should have dev chart loaded", func() {
-			chart, err := helmutils.GetEmbeddedChart("dev")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(chart).NotTo(BeNil())
-
-			// our test charts dev is v0.11.0-rc.1
-			Expect(chart.AppVersion()).To(Equal("0.11.0-rc.1"))
+			// Charts for acm 2.13 should be volsync v0.12.z
+			Expect(strings.HasPrefix(chart.AppVersion(), "0.12.")).To(BeTrue())
 		})
 
 		It("Should not have other charts loaded", func() {
@@ -50,15 +44,20 @@ var _ = Describe("Helmutils", func() {
 		var renderedObjs []runtime.Object
 		//var chartKey string
 
+		var chartValues map[string]interface{}
+
+		BeforeEach(func() {
+			chartValues = map[string]interface{}{}
+		})
+
 		JustBeforeEach(func() {
-			chart, err := helmutils.GetEmbeddedChart("stable")
+			chart, err := helmutils.GetEmbeddedChart(controllers.DefaultHelmChartKey)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(chart).NotTo(BeNil())
 
 			testCluster := &clusterv1.ManagedCluster{
 				ObjectMeta: metav1.ObjectMeta{},
 			}
-			chartValues := map[string]interface{}{}
 
 			renderedObjs, err = helmutils.RenderManifestsFromChart(chart, testNamespace, testCluster, clusterIsOpenShift,
 				chartValues, genericCodec)
@@ -73,7 +72,7 @@ var _ = Describe("Helmutils", func() {
 			})
 
 			It("Should render the helm chart", func() {
-				verifyHelmRenderedVolSyncObjects(renderedObjs, testNamespace, clusterIsOpenShift)
+				helmutilstest.VerifyHelmRenderedVolSyncObjects(renderedObjs, testNamespace, clusterIsOpenShift)
 			})
 		})
 
@@ -84,126 +83,39 @@ var _ = Describe("Helmutils", func() {
 			})
 
 			It("Should render the helm chart", func() {
-				verifyHelmRenderedVolSyncObjects(renderedObjs, testNamespace, clusterIsOpenShift)
+				helmutilstest.VerifyHelmRenderedVolSyncObjects(renderedObjs, testNamespace, clusterIsOpenShift)
+			})
+
+			When("custom chart values are provided", func() {
+				myNodeSelector := map[string]string{
+					"selecta": "selectorvaluea",
+					"selectb": "selectorvalueb",
+				}
+
+				myTolerations := []corev1.Toleration{
+					{
+						Key:      "node.kubernetes.io/unreachable",
+						Operator: corev1.TolerationOpExists,
+						Effect:   corev1.TaintEffectNoSchedule,
+					},
+				}
+
+				BeforeEach(func() {
+					// Set some chart values
+					chartValues["nodeSelector"] = myNodeSelector
+					chartValues["tolerations"] = myTolerations
+				})
+
+				It("Should render the helm chart using the values", func() {
+					volsyncDeploy := helmutilstest.VerifyHelmRenderedVolSyncObjects(renderedObjs, testNamespace, clusterIsOpenShift)
+					logger.Info("deployment", "volsyncDeploy", &volsyncDeploy)
+					Expect(volsyncDeploy.Spec.Template.Spec.NodeSelector).NotTo(BeNil())
+					Expect(volsyncDeploy.Spec.Template.Spec.NodeSelector).To(Equal(myNodeSelector))
+
+					Expect(volsyncDeploy.Spec.Template.Spec.Tolerations).NotTo(BeNil())
+					Expect(volsyncDeploy.Spec.Template.Spec.Tolerations).To(Equal(myTolerations))
+				})
 			})
 		})
 	})
 })
-
-//nolint:funlen
-func verifyHelmRenderedVolSyncObjects(objs []runtime.Object, testNamespace string, clusterIsOpenShift bool) {
-	// Check objects
-	// There should be:
-	// - 2 CRDs (replicationsource, replicationdestination)
-	// - 3 clusterroles
-	//   - 1 for the manager controller
-	//   - 1 for metrics reader
-	//   - 1 for proxy
-	// - 2 clusterrolebindings
-	//   - 1 for the manager controller
-	//   - 1 for proxy
-	// - 1 role (leader election)
-	// - 1 rolebinding (leader election)
-	// - 1 deployment (volsync)
-	// - 1 service (metrics)
-	// - 1 serviceaccount (volsync)
-	Expect(len(objs)).To(Equal(12))
-
-	crds := []*apiextensionsv1.CustomResourceDefinition{}
-	clusterRoles := []*rbacv1.ClusterRole{}
-	clusterRoleBindings := []*rbacv1.ClusterRoleBinding{}
-	var role *rbacv1.Role
-	var roleBinding *rbacv1.RoleBinding
-	var deployment *appsv1.Deployment
-	var service *corev1.Service
-	var serviceAccount *corev1.ServiceAccount
-
-	for _, obj := range objs {
-		objKind := obj.GetObjectKind().GroupVersionKind().Kind
-
-		klog.InfoS("Object kind", "objKind", objKind)
-
-		switch objKind {
-		case "CustomResourceDefinition":
-			crd, ok := obj.(*apiextensionsv1.CustomResourceDefinition)
-			Expect(ok).To(BeTrue())
-			crds = append(crds, crd)
-		case "ClusterRole":
-			clusterRole, ok := obj.(*rbacv1.ClusterRole)
-			Expect(ok).To(BeTrue())
-			clusterRoles = append(clusterRoles, clusterRole)
-		case "ClusterRoleBinding":
-			clusterRoleBinding, ok := obj.(*rbacv1.ClusterRoleBinding)
-			Expect(ok).To(BeTrue())
-			clusterRoleBindings = append(clusterRoleBindings, clusterRoleBinding)
-		case "Role":
-			r, ok := obj.(*rbacv1.Role)
-			Expect(ok).To(BeTrue())
-			role = r
-		case "RoleBinding":
-			rb, ok := obj.(*rbacv1.RoleBinding)
-			Expect(ok).To(BeTrue())
-			roleBinding = rb
-		case "Deployment":
-			d, ok := obj.(*appsv1.Deployment)
-			Expect(ok).To(BeTrue())
-			deployment = d
-		case "Service":
-			s, ok := obj.(*corev1.Service)
-			Expect(ok).To(BeTrue())
-			service = s
-		case "ServiceAccount":
-			sa, ok := obj.(*corev1.ServiceAccount)
-			Expect(ok).To(BeTrue())
-			serviceAccount = sa
-		}
-	}
-
-	Expect(len(crds)).To(Equal(2))
-	Expect(len(clusterRoles)).To(Equal(3))
-	Expect(len(clusterRoleBindings)).To(Equal(2))
-	Expect(role).NotTo(BeNil())
-	Expect(roleBinding).NotTo(BeNil())
-	Expect(deployment).NotTo(BeNil())
-	Expect(service).NotTo(BeNil())
-	Expect(serviceAccount).NotTo(BeNil())
-
-	// Check CRDs
-	foundReplicationSourceCRD := false
-	foundReplicationDestinationCRD := false
-	for _, crd := range crds {
-		if crd.GetName() == "replicationsources.volsync.backube" {
-			foundReplicationSourceCRD = true
-		} else if crd.GetName() == "replicationdestinations.volsync.backube" {
-			foundReplicationDestinationCRD = true
-		}
-	}
-	Expect(foundReplicationSourceCRD).To(BeTrue())
-	Expect(foundReplicationDestinationCRD).To(BeTrue())
-
-	// Check namespace on namespaced resources is set correctly
-	namespacedObjs := []metav1.Object{
-		role,
-		roleBinding,
-		deployment,
-		service,
-		serviceAccount,
-	}
-	for _, nsObj := range namespacedObjs {
-		Expect(nsObj.GetNamespace()).To(Equal(testNamespace))
-	}
-
-	// Check deployment
-	Expect(len(deployment.Spec.Template.Spec.Containers)).To(Equal(2))
-	Expect(deployment.Spec.Template.Spec.Containers[0].Name).To(Equal("kube-rbac-proxy"))
-	Expect(deployment.Spec.Template.Spec.Containers[1].Name).To(Equal("manager"))
-	//TODO: check images?
-	Expect(deployment.Spec.Template.Spec.ServiceAccountName).To(Equal(serviceAccount.GetName()))
-	if clusterIsOpenShift {
-		// RunAsUser should not be set for OpenShift, openshift will set this
-		Expect(deployment.Spec.Template.Spec.SecurityContext.RunAsUser).To(BeNil())
-	} else {
-		Expect(deployment.Spec.Template.Spec.SecurityContext.RunAsUser).NotTo(BeNil())
-		Expect(*deployment.Spec.Template.Spec.SecurityContext.RunAsUser).To(Equal(int64(65534)))
-	}
-}
